39bcba9730fee110ea3b7e7184a214319e9dcee9
Totsnuk <evanmaywald2@gmail.com>
2024-12-23 02:41:30 -0600

    fixed lsmrr.lua and removed bloat

--- mods/Louder Noisemakers + Extended Range Remotes/42/media/lua/server/LSMRR.lua
+++ mods/Louder Noisemakers + Extended Range Remotes/42/media/lua/server/LSMRR.lua
@@ -1,37 +1,36 @@
---- Item properties are removed during crafting, so how can we modify them in a way that my modified properties are kept?
---- When a louder item is used in crafting, convert new item into its louder counterpart, if applicable.
+-- Item properties are removed during crafting, so how can we modify them in a way that my modified properties are kept?
+-- When a louder item is used in crafting, convert new item into its louder counterpart, if applicable.
 
---- It may be that when you have an item favorited it saves the modData, meaning it would save the louder sound range, will have to test
+-- It may be that when you have an item favorited it saves the modData, meaning it would save the louder sound range, will have to test
 
---- make louder and return indentifier for kind of item
-function MakeLouder(item, itemName)
-    if string.find(itemName, "Digital") and string.find(itemName, "Watch") then
+-- make louder and return indentifier for kind of item
+
+local Recipe = Recipe;
+
+function MakeLouder(item, scriptItemName)
+    if string.find(scriptItemName, "Watch") and string.find(scriptItemName, "Digital") then
         item:setSoundRadius(25);
         return "radius";
-    elseif string.find(itemName, "Alarm") and string.find(itemName, "Clock") then
+    elseif string.find(scriptItemName, "AlarmClock2") then
         item:setSoundRadius(50);
         return "radius";
-    elseif string.find(itemName, "Noise") and (string.find(itemName, "Maker") or string.find(itemName, "Generator")) then
+    elseif string.find(scriptItemName, "NoiseTrap") then
         item:setNoiseRange(150);
         return "noise";
-    elseif string.find(itemName, "Remote") and string.find(itemName, "Controller") then
-        if string.find(itemName, "V1") then
+    elseif string.find(scriptItemName, "RemoteCrafted") then
+        if string.find(scriptItemName, "V1") then
             item:setRemoteRange(50);
             return "remote";
-        elseif string.find(itemName, "V2") then
+        elseif string.find(scriptItemName, "V2") then
             item:setRemoteRange(75);
             return "remote";
-        elseif string.find(itemName, "V3") then
+        elseif string.find(scriptItemName, "V3") then
             item:setRemoteRange(100);
             return "remote";
         end
     end
     return false;
 end
-
---- dont need to test for modData bc the func for it already has a case for that
-
-local Recipe = Recipe;
 
 local louderStr = "(Louder)";
 local erStr = "Extended Range";
@@ -55,48 +54,58 @@ function Recipe.OnCreate.LSMRR_MakeLouder(craftRecipeData, _)
     -- end
 
     local itemName = inventoryItem:getName();
+    if itemName == nil then
+        print("Recipe.OnCreate.LSMRR_MakeLouder: itemName is nil");
+        return;
+    end
     print("Recipe.OnCreate.LSMRR_MakeLouder itemName: " .. itemName);
-    local itemCustomType = MakeLouder(inventoryItem, itemName);
+    local scriptItem = inventoryItem:getScriptItem() --- script template for item
+    print("Recipe.OnCreate.LSMRR_MakeLouder scriptItem: " .. tostring(scriptItem));
+    local scriptItemName = scriptItem:getName();
+    
+    local itemCustomType = MakeLouder(inventoryItem, scriptItemName);
     print("Recipe.OnCreate.LSMRR_MakeLouder itemCustomType: " .. itemCustomType);
-
    
     print("Recipe.OnCreate.LSMRR_MakeLouder modData: " .. tostring(modData));
     local newItemName = "";
     
-    --- check if item is valud and modify accordingly
-    if itemCustomType == "radius" or itemCustomType == "noise" then
+    --- check if item is valid and modify accordingly
+    if itemCustomType == "radius" then
         modData.LSMRR_isLouder = true;
         if not string.find(itemName, louderStr) then
             newItemName = itemName .. " " .. louderStr;
         end
+        print("Recipe.OnCreate.LSMRR_MakeLouder soundRadius: " .. tostring(inventoryItem:getSoundRadius()));
+    elseif itemCustomType == "noise" then
+        modData.LSMRR_isNoisier = true;
+        if not string.find(itemName, louderStr) then
+            newItemName = itemName .. " " .. louderStr;
+        end
+        print("Recipe.OnCreate.LSMRR_MakeLouder noiseRange: " .. tostring(inventoryItem:getNoiseRange()));
     elseif itemCustomType == "remote" then
         modData.LSMRR_hasExtendedRange = true;
         if not string.find(itemName, erStr) then
             newItemName =  erStr .. " " .. itemName;
         end
+        --print("Recipe.OnCreate.LSMRR_MakeLouder remoteRange: " .. tostring(inventoryItem:getRemoteRange()));
     else
-        print("LSMRR Recipe.OnCreate: itemCustomType is not valid");
+        --print("LSMRR Recipe.OnCreate: itemCustomType is not valid");
         return;
     end
-    print("Recipe.OnCreate.LSMRR_MakeLouder newItemName: " .. newItemName);
+    --- make sure newItemName isnt an empty string
+    if newItemName == "" then
+        newItemName = itemName;
+    end
+    --print("Recipe.OnCreate.LSMRR_MakeLouder newItemName: " .. newItemName);
 
     --- set names
     if not inventoryItem:isCustomName() then
         inventoryItem:setCustomName(true);
     end
-    inventoryItem:setName(newItemName);
-    print("Recipe.OnCreate.LSMRR_MakeLouder inventoryItem:getName(): " .. inventoryItem:getName());
-    ---inventoryItem:setDisplayName(newItemName);
-    ---print("Recipe.OnCreate.LSMRR_MakeLouder inventoryItem:getDisplayName(): " .. inventoryItem:getDisplayName());
+    if newItemName ~= nil then
+        inventoryItem:setName(newItemName);
+        --print("Recipe.OnCreate.LSMRR_MakeLouder inventoryItem:getName(): " .. inventoryItem:getName());
+    end
+    --inventoryItem:setDisplayName(newItemName);
+    --print("Recipe.OnCreate.LSMRR_MakeLouder inventoryItem:getDisplayName(): " .. inventoryItem:getDisplayName());
 end
-
----if item is being crafted, and item is related to this mod
----timer_for_recipe = recipe_craft_time * 10
-    ---when timer ends, stop checking for signals
----hook into signals
-    ---new craft (recipe)
-        ---store ref to recipe
-    ---ingredient being deleted (itembeingdeleted)
-        ---save itembeingdeleted modData filtered to only my stuff
-    ---signal for new item being created (newitem)
-        ---if newitem is my item, apply modData to it and stop listening for signals
