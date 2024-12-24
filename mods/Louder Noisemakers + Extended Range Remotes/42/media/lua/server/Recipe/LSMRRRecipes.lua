fb583cbdf8b2e5e40ee5d1bd5e052f46431912f1
Totsnuk <evanmaywald2@gmail.com>
2024-12-24 07:25:06 -0600

    Update LSMRRRecipes.lua

--- mods/Louder Noisemakers + Extended Range Remotes/42/media/lua/server/Recipe/LSMRRRecipes.lua
+++ mods/Louder Noisemakers + Extended Range Remotes/42/media/lua/server/Recipe/LSMRRRecipes.lua
@@ -1,20 +1,17 @@
-require 'server/LSMRR';
-
-local LSMRR = LSMRR;
 local Recipe = Recipe;
 
 function Recipe.OnCreate.LSMRR_BoostWatchVolume(craftRecipeData, character)
-    LSMRR.OnMakeLouder(craftRecipeData, character, "Radius");
+    LSMRRMain.OnMakeLouder(craftRecipeData, character, "Radius");
 end
 function Recipe.OnCreate.LSMRR_AdjustGearsOnAlarmClock(craftRecipeData, character)
-    LSMRR.OnMakeLouder(craftRecipeData, character, "Radius");
+    LSMRRMain.OnMakeLouder(craftRecipeData, character, "Radius");
 end
 function Recipe.OnCreate.LSMRR_ModulateNoiseMakerVolume(craftRecipeData, character)
-    LSMRR.OnMakeLouder(craftRecipeData, character, "Noise");
+    LSMRRMain.OnMakeLouder(craftRecipeData, character, "Noise");
 end
 function Recipe.OnCreate.LSMRR_ExtendRangeOfRemoteController(craftRecipeData, character)
-    LSMRR.OnMakeLouder(craftRecipeData, character, "Remote");
+    LSMRRMain.OnMakeLouder(craftRecipeData, character, "Remote");
 end
 function Recipe.OnCreate.LSMRR_AttachAmplifierToNoiseMaker(craftRecipeData, character)
-    LSMRR.OnMakeLouder(craftRecipeData, character, "Noise");
+    LSMRRMain.OnMakeLouder(craftRecipeData, character, "Noise");
 end
