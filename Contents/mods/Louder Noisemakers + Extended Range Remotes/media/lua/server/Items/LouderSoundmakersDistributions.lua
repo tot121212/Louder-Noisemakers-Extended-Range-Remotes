5e56c283ad6e2eb10ea81006c81d44137de146b0
Totsnuk <evanmaywald2@gmail.com>
2024-12-30 16:24:16 -0600

    b41 hotfix

--- Contents/mods/Louder Noisemakers + Extended Range Remotes/media/lua/server/Items/LouderSoundmakersDistributions.lua
+++ Contents/mods/Louder Noisemakers + Extended Range Remotes/media/lua/server/Items/LouderSoundmakersDistributions.lua
@@ -1,6 +1,4 @@
-require 'Items/ProceduralDistributions'
-
-local default_table = ProceduralDistributions;
+local default_table = ProceduralDistributions
 
 --- The pass by ref data for default literature chance
 local default_literature_chances = {
@@ -17,26 +15,26 @@ local default_literature_chances = {
     ["MagazineRackMixed"]           = 1,
     ["PostOfficeMagazines"]         = 1,
     ["ShelfGeneric"]                = 0.1,
-    ["ToolStoreBooks"]              = 2
+    ["ToolStoreBooks"]              = 2,
 }
 
 local literature_names_with_chances = {
     ["4X_Range_Remotes_Mag_LOUDER_Tot"] = default_literature_chances,
-    ["NoiseTrap_Mag_LOUDER_Tot"] = default_literature_chances
+    ["NoiseTrap_Mag_LOUDER_Tot"] = default_literature_chances,
 }
 
-local literature_module_name = "LouderSoundmakers_Items_Literature";
+local literature_module_name = "LouderSoundmakers_Items_Literature"
 
 --- Iterate items and item_names and add to _table with module_name prepended
 --- Should make a seperate list with the distributions but... :D
 local function iterate_items_into_table(items, module_name, _table)
-    for item_name, _ in pairs(items) do
+    for item_name, v in pairs(items) do
         for distribution, chance_for_item in pairs(items[item_name]) do
-            table.insert(_table["list"][distribution].items, module_name .. item_name);
-            table.insert(_table["list"][distribution].items, chance_for_item);
+            table.insert(_table["list"][distribution].items, module_name .. " " .. item_name)
+            table.insert(_table["list"][distribution].items, chance_for_item)
         end
     end
 end
 
 --- Add literature to default_table
-iterate_items_into_table(literature_names_with_chances, literature_module_name, default_table);
+iterate_items_into_table(literature_names_with_chances, literature_module_name, default_table)
